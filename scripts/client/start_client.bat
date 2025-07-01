df7eeb5b06c3e42f4dc907b86ba430b2f141d981
JCarlos251 <juanncvq@outlook.com>
2025-07-01 18:04:41 -0300

    cliente - orientação a objeto

--- scripts/client/start_client.bat
+++ scripts/client/start_client.bat
@@ -2,5 +2,5 @@
 REM ALTERAR O CAMINHO ABAIXO PARA SUA PASTA RAIZ DO PROJETO.
 cd /d C:\Users\juann\Documents\bigfs-v3
 echo [Script] Iniciando Cliente...
-python -m cliente.main
+python -m cliente.main_cliente
 pause
