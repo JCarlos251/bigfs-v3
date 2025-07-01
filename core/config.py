7530562020407bc6525b9a443223c30d07949e20
lucasvitorrt <lvitor82@gmail.com>
2025-06-30 22:14:44 -0300

    organizações e benchmarks

--- core/config.py
+++ core/config.py
@@ -1,6 +1,6 @@
 # CHUNK_SIZE, REPLICATION_FACTOR, PATHS
 
-CHUNK_SIZE = 1 * 1024 * 1024  # 1 MB por chunk
+CHUNK_SIZE = 10 * 1024 * 1024  # 1 MB por chunk
 REPLICATION_FACTOR = 2         # Número de cópias de cada chunk
 HEARTBEAT_INTERVAL = 3         # Segundos entre cada heartbeat
 HEARTBEAT_TIMEOUT = 15        # 10 minutos = 600 segundos
